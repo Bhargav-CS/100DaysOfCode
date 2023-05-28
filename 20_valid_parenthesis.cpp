c9fbf7872c19f7bf5530cc2d4006eb5772b2ceb7
Bhargav Patki <bhargavpatki@gmail.com>
2023-05-28 23:15:23 +0530

    day 38, leetcode 20

---
+++ 20_valid_parenthesis.cpp
@@ -0,0 +1,28 @@
+#include <stack>
+#include <unordered_map>
+#include <string>
+
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> st;
+        std::unordered_map<char, char> mappings = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (st.empty() || st.top() != mappings[c]) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};

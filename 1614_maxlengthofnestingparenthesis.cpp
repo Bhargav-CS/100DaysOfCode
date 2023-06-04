342a07c97ec8ce294b5fcc1b153f5a84fca0973a
Bhargav Patki <bhargavpatki@gmail.com>
2023-06-04 22:39:12 +0530

    day 45, leetcode 1614 max length of the nested parenthesis

---
+++ 1614_maxlengthofnestingparenthesis.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+    int maxDepth = 0;
+    int currentDepth = 0;
+    
+    for (char c : s) {
+        if (c == '(') {
+            currentDepth++;
+            maxDepth = max(maxDepth, currentDepth);
+        } else if (c == ')') {
+            currentDepth--;
+        }
+    }
+    
+    return maxDepth;
+}
+
+};

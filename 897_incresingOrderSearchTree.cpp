d5879c88b8ff45251496260d75f73a5c8cb041fc
Bhargav Patki <bhargavpatki@gmail.com>
2023-06-15 21:57:50 +0530

    day 56, leetcode 897

---
+++ 897_incresingOrderSearchTree.cpp
@@ -0,0 +1,36 @@
+
+// Definition for a binary tree node.
+//leetcode 897 - incresing order search tree
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* dummy = new TreeNode(-1);
+        TreeNode* curr = dummy;
+        
+        inorder(root, curr);
+        
+        return dummy->right;
+    }
+    
+    void inorder(TreeNode* node, TreeNode*& curr) {
+        if (node == nullptr) {
+            return;
+        }
+        
+        inorder(node->left, curr);
+        
+        curr->right = new TreeNode(node->val);
+        curr = curr->right;
+        
+        inorder(node->right, curr);
+    }
+};
